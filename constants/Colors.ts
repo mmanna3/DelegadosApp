b206bdb1cce0ccf40a11acefe2ab22f811abbd62
mmanna3 <mmmannna3@gmail.com>
2022-07-19 10:39:09 -0300

    Mejora formato de credencial

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,6 +5,7 @@ export default {
   fondoDeTexto: "rgba(210, 210, 210, 0.6)",
   verde: "#01aa59",
   azul: "#0038ba",
+  rojo: "#e81f05",
   light: {
     text: "#000",
     background: "#fff",
--- screens/JugadoresScreen/components/JugadorCard.tsx
+++ screens/JugadoresScreen/components/JugadorCard.tsx
@@ -1,28 +1,51 @@
 import React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
+import Colors from "../../../constants/Colors";
 
 interface Props {
   jugador: IJugador;
 }
 
 export default function JugadorCard(props: Props) {
+  const primeraMayuscRestoMinusc = (texto: string) => {    
+    const palabras = texto.split(" ");
+    let resultado = "";
+    
+    palabras.forEach((palabra) => {
+      resultado += `${palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase() + " "}`;
+    })
+    
+    return resultado;    
+  }
+
   const { jugador } = props;
   return (
     <View style={styles.card}>
       {jugador.EstaSuspendido && (
         <Text style={styles.datoSuspendido}>JUGADOR SUSPENDIDO</Text>
       )}
-      <Text style={styles.equipoYTorneo}>
-        {jugador.Equipo} - {jugador.TipoLiga}
+      <Text style={styles.equipo}>
+        {primeraMayuscRestoMinusc(jugador.Equipo)}
+      </Text>
+      <Text style={styles.tipoLiga}>
+        {primeraMayuscRestoMinusc(jugador.TipoLiga)}
       </Text>
       <Image
         style={styles.foto}
         source={{ uri: `data:image/jpeg;base64,${jugador.FotoBase64}` }}
       />
-      <Text style={styles.dato}>{jugador.DNI}</Text>
-      <Text style={styles.dato}>{jugador.Nombre}</Text>
-      <Text style={styles.dato}>{jugador.Apellido}</Text>
-      <Text style={styles.dato}>{jugador.FechaNacimiento}</Text>
+      <View style={styles.datosContenedor}>
+        <View>
+          <Text style={styles.datoVerde}>{jugador.DNI}</Text>
+          <Text style={styles.datoAzul}>{jugador.Nombre}</Text>
+          <Text style={styles.datoAzul}>{jugador.Apellido}</Text>
+          <Text style={styles.datoRojo}>{jugador.FechaNacimiento}</Text>
+        </View>        
+      </View>
+      <Image
+          source={require("../../../assets/images/logo.png")}
+          style={styles.logo}
+        />
     </View>
   );
 }
@@ -44,20 +67,29 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: 300,
     height: 430,
-    borderRadius: 10,
+    borderRadius: 20,
     paddingVertical: 25,
     paddingHorizontal: 32,
     elevation: 6,
-    backgroundColor: "#01aa59",
+    borderColor: Colors.azul,
+    borderWidth: 1.5,
     justifyContent: "center",
     alignItems: "center",
   },
-  equipoYTorneo: {
+  equipo: {
     fontSize: 19,
     lineHeight: 21,
     fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
+    letterSpacing: 0.40,
+    color: Colors.verde,
+  },
+  tipoLiga: {
+    fontSize: 19,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.40,
+    color: Colors.verde,
+    marginTop: 5,
   },
   datoSuspendido: {
     marginBottom: 30,
@@ -74,13 +106,29 @@ const styles = StyleSheet.create({
     paddingHorizontal: 4,
     backgroundColor: "red",
   },
-  dato: {
+  datoRojo: {
     marginBottom: 3,
     fontSize: 17,
     lineHeight: 21,
     fontWeight: "bold",
     letterSpacing: 0.4,
-    color: "white",
+    color: Colors.rojo,
+  },
+  datoVerde: {
+    marginBottom: 3,
+    fontSize: 17,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.4,
+    color: Colors.verde,
+  },
+  datoAzul: {
+    marginBottom: 3,
+    fontSize: 17,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.4,
+    color: Colors.azul,
   },
   foto: {
     marginVertical: 15,
@@ -88,6 +136,18 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 10,
     borderWidth: 2,
-    borderColor: "#ccc",
+    borderColor: Colors.azul,
   },
+  logo: {
+    width: 70,
+    height: 70,
+    position: 'absolute',
+    top: 350,
+    left: 220,
+    opacity: 0.5,
+  },
+  datosContenedor: {
+    display: "flex",
+    flexDirection: "row",
+  }
 });
