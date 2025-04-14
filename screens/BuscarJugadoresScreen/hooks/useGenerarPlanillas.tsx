6caed13a735c21871d000a1e293bff1988d47db0
mmanna3 <mmmannna3@gmail.com>
2025-04-13 23:28:38 -0300

    acomoda planillas

--- screens/BuscarJugadoresScreen/hooks/useGenerarPlanillas.tsx
+++ screens/BuscarJugadoresScreen/hooks/useGenerarPlanillas.tsx
@@ -1,9 +1,13 @@
+import * as FileSystem from "expo-file-system";
+import * as Print from "expo-print";
+import { shareAsync } from "expo-sharing";
 import { useState } from "react";
+import { esteMismisimoMomento } from "../support/generarHtmlParaPdf";
 import useGetPlanillas from "./useGetPlanillas";
 
 const useGenerarPlanillas = () => {
   const [estaGenerandoPDF, setEstaGenerandoPDF] = useState(false);
-  const { getPlanillas, isLoading } = useGetPlanillas();
+  const { getPlanillas, isLoading: estaHaciendoElRequest } = useGetPlanillas();
 
   const generarPlanillas = async (codigoAlfanumerico: string) => {
     try {
@@ -13,24 +17,24 @@ const useGenerarPlanillas = () => {
 
       console.log(planillas);
 
-      // setEstaGenerandoPDF(true);
-      // const { uri } = await Print.printToFileAsync({
-      //   html: generarHtml(planillas),
-      // });
-      // const pdfRenombrado = `${uri.slice(
-      //   0,
-      //   uri.lastIndexOf("/") + 1
-      // )}planillas_${esteMismisimoMomento()}.pdf`;
+      setEstaGenerandoPDF(true);
+      const { uri } = await Print.printToFileAsync({
+        html: generarPlanillasHtml(planillas),
+      });
+      const pdfRenombrado = `${uri.slice(
+        0,
+        uri.lastIndexOf("/") + 1
+      )}planillas_${esteMismisimoMomento()}.pdf`;
 
-      // await FileSystem.moveAsync({
-      //   from: uri,
-      //   to: pdfRenombrado,
-      // });
+      await FileSystem.moveAsync({
+        from: uri,
+        to: pdfRenombrado,
+      });
 
-      // await shareAsync(pdfRenombrado, {
-      //   UTI: ".pdf",
-      //   mimeType: "application/pdf",
-      // });
+      await shareAsync(pdfRenombrado, {
+        UTI: ".pdf",
+        mimeType: "application/pdf",
+      });
     } catch (error) {
       console.error(error);
     } finally {
@@ -38,7 +42,10 @@ const useGenerarPlanillas = () => {
     }
   };
 
-  return { generarPlanillas, isLoading: estaGenerandoPDF };
+  return {
+    generarPlanillas,
+    isLoading: estaGenerandoPDF || estaHaciendoElRequest,
+  };
 };
 
 export default useGenerarPlanillas;
